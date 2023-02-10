0a0ca093e51ec32b1e1504220d13ffe9a5d98cf5
abu <abusirus@gmail.com>
2023-02-10 23:08:07 +0530

    add youtude and poster click action to trailer

--- src/components/RowPost/RowPost.js
+++ src/components/RowPost/RowPost.js
@@ -1,23 +1,49 @@
 import axios from "../../axios";
 import React, { useEffect, useState } from "react";
 import "./RowPost.css";
-import { imageUrl } from "../../constants/constants";
+import { API_KEY, imageUrl } from "../../constants/constants";
 import YouTube from "react-youtube";
 
 function RowPost(props) {
   const [movie, setMovie] = useState([]);
+  const [urlId, setUrlId] = useState('');
 
+  //get TMDB data collections
   useEffect(() => {
     axios
       .get(props.url)
       .then((response) => {
-        console.log(response.data);
+        // console.log(response.data);
         setMovie(response.data.results);
       })
       .catch((err) => {
         console.log(err);
       });
   }, [props]);
+
+  //youtube size
+  const opts = {
+    height: '500',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleMovie = (id)=>{
+    console.log(id);
+    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
+      // console.log(response.data);
+      if(response.data.results.length !==0){
+        setUrlId(response.data.results[0])
+      }else{
+        console.log('Array Empty');
+      }
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
 
   return (
     <div className="ROWPOST">
@@ -31,10 +57,12 @@ function RowPost(props) {
               alt="loading"
               className={props.isSmall ? "small-poster" : "poster"}
               key={obj.id}
+              onClick={()=>handleMovie(obj.id)}
             />
           ))}
         </div>
-        {/* <YouTube /> */}
+        {/* auto Play vedio */}
+        { urlId && <YouTube opts={opts} videoId={urlId.key} /> }
       </div>
     </div>
   );
